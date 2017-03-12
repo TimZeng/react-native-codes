e1909962c36ab488d2a12c94a2e7dc68fc194986
Tim Zeng <zengtiantian1122@gmail.com>
2017-03-12 14:48:54 -0700

    add state to LoginForm component

--- auth/src/components/LoginForm.js
+++ auth/src/components/LoginForm.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { TextInput } from 'react-native';
+
+import { Button, Card, CardSection } from './common';
 
 class LoginForm extends Component {
+  state = { text: '' };
+
   render() {
     return (
-      <View />
+      <Card>
+        <CardSection>
+          <TextInput
+            value={this.state.text}
+            style={{ height: 20, width: 100 }}
+            onChangeText={text => this.setState({ text })}
+          />
+        </CardSection>
+
+        <CardSection />
+
+        <CardSection>
+          <Button>
+            Log in
+          </Button>
+        </CardSection>
+      </Card>
     );
   }
 }
