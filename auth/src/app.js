c5faf464573ced963712831261a2f895b2ff6781
Tim Zeng <zengtiantian1122@gmail.com>
2017-03-12 14:13:23 -0700

    install google firebase package and initialize firebase application when app component is about to be rendered

--- auth/src/app.js
+++ auth/src/app.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
 
 import { Header } from './components/common';
 
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyBJwUTqgcOwC_aTxtrJHOYOomXPENa-zdY',
+      authDomain: 'authentication-42a05.firebaseapp.com',
+      databaseURL: 'https://authentication-42a05.firebaseio.com',
+      storageBucket: 'authentication-42a05.appspot.com',
+      messagingSenderId: '422512772919'
+    });
+  }
+
   render() {
     return (
       <View>
